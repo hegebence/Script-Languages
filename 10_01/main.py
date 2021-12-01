41c4faa63a145cd287652f90601a7c040881ba1f
hegebence <h.bencejozsef@gmail.com>
2021-12-01 17:47:58 +0100

    Egy crash miatt eltűnt a repository

---
+++ 10_01/main.py
@@ -0,0 +1,9 @@
+print("Please write your name: ")
+name = input()
+
+print("Please write your age: ")
+age = int(input())
+
+print("Your name: {0}, age: {1}".format(name, age))
+
+print("Next year you'll be {}".format(age+1))
